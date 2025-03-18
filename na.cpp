ee5157f72cac14afd09ebd3b589c9a7db0b22318
nati1216 <natiyabi790@gmail.com>
2025-03-18 12:05:14 -0700

    initial commit

---
+++ na.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int binarySearchIterative(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search the right half
+        } else {
+            right = mid - 1; // Search the left half
+        }
+    }
+    return -1; // Target not found
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = binarySearchIterative(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found." << endl;
+    }
+
+    return 0;
